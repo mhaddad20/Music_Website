b5976e568ccd6c44501893817df4b87250125ff3
Muhammed <muhammedhaddad2@gmail.com>
2021-09-27 21:02:12 +0100

    part44

--- shop/views.py
+++ shop/views.py
@@ -14,10 +14,14 @@ def allProdCat(request, category_id=None):
     
     """Pagination code"""
     paginator = Paginator(products_list, 6)
-    page = int(request.GET.get('page','1'))
-    products = paginator.page(page)
-    #products = paginator.page(paginator.num_pages)
-
+    try:
+        page = int(request.GET.get('page','1'))
+    except:
+        page = 1
+    try:
+        products = paginator.page(page)
+    except (EmptyPage,InvalidPage):
+        products = paginator.page(paginator.num_pages)
     return render(request, 'shop/category.html', {'category':c_page, 'products':products})
 
 
